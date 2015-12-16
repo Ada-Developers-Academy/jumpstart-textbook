bcdef54fec346086062b8ef02389be1c0a06104c
jnf <jeremy.flores@gmail.com>
2015-12-16 10:41:53 -0800

    UI/UX for the global page header. Includes markup and styles for mobile and desktop.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -8,4 +8,17 @@ $(document).ready(function(){
 
     e.preventDefault();
   });
+
+  //top nav menu toggle when in mobile layout
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
 });
