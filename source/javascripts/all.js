1c3cb1e13233186c83860c352947205d376c6b9d
jnf <jeremy.flores@gmail.com>
2015-12-16 10:10:59 -0800

    Force ToC scroll to top when closing panel so we don't lose sight of the hamburger trigger. Also, scroll the actual contents instead of the entire panel, leaving the hamburger and title at the top of the page.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
+  //table of contents menu toggling
   $('.toc-menu,.toc-fade').on('click touchstart',function (e) {
     $('.toc,.toc-fade').toggleClass('is-visible');
+    if( !$('.toc').hasClass('is-visible')) {
+      $('.toc-content').prop({ scrollTop: 0 });
+    }
+
     e.preventDefault();
   });
 });
