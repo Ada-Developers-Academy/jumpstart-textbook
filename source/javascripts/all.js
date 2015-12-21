ba701176bf6a310358ad890e067f63d7fd7b55b1
jnf <jeremy.flores@gmail.com>
2015-12-20 15:06:42 -1000

    Hide lectures not from the current chapter in the toc. Use a click/touch event to toggle lecture list.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   //table of contents menu toggling
-  $('.toc-menu,.toc-fade').on('click touchstart',function (e) {
+  $('.toc-menu,.toc-fade').on('click touchstart', function(e) {
     $('.toc,.toc-fade').toggleClass('is-visible');
     if( !$('.toc').hasClass('is-visible')) {
       $('.toc-content').prop({ scrollTop: 0 });
@@ -8,6 +8,15 @@ $(document).ready(function(){
 
     e.preventDefault();
   });
+
+  //table of contents chapter list toggling
+  $('.toc-chapter').on('click touchstart', function(e) {
+    $(this)
+      .toggleClass('is-current')
+      .next('.toc-lectures').toggleClass('is-current');
+
+    e.preventDefault();
+  })
 
   //top nav menu toggle when in mobile layout
   var menuToggle = $('#js-mobile-menu').unbind();
